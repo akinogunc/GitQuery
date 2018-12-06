76101cbbe730010b93a87aee104ac92184fa5aab
Maruf Nebil Ogunc <home@marufs-imac.local>
2018-12-06 20:40:14 +0300

    Initial Commit

---
+++ GitQuery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitQuery
+//
+//  Created by Maruf Nebil Ogunc on 6.12.2018.
+//  Copyright © 2018 Maruf Nebil Ogunc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
